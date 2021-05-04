24a5d332ff662b12859b1ebe5695ae19b2e8518c
Christer <christerforsgren91@gmail.com>
2021-05-04 19:14:59 +0200

    configured CORS in cors.rb deleted it from application.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       expose: %w[access-token expiry token-type uid client],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      max_age: 0
   end
 end
