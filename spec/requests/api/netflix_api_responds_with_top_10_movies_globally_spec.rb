072aaee5812b115d9d01a449e278d566b7df5d29
DavDan1 <david-danielyan@hotmail.com>
2021-05-05 11:23:46 +0200

    add first test generates controller movies changes json fixture from top 10 to top 100 set up routes

---
+++ spec/requests/api/netflix_api_responds_with_top_10_movies_globally_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'GET /api/movies', type: :request do
+  let(:top_100_response) do
+    file_fixture('top_movie_search_api_response.json').read
+  end
+
+  describe 'receives top 100 movies globally' do
+    before do
+      stub_request(:get, "https://unogsng.p.rapidapi.com/search?type=movie&orderby=rating")
+      .to_return(status: 200, body: top_100_response)
+      get '/api/movies'
+    end
+
+    it 'is expected to respond 200' do
+      expect(response).to have_http_status 200
+    end
+  end
+end
