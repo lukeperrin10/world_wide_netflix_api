b59bf459cdcb7bb4463f8e2c5a987ec7d9e4d0a6
luke <lukeperrin27@gmail.com>
2021-05-08 13:58:46 +0200

    refactors movies controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -3,26 +3,30 @@ class Api::MoviesController < ApplicationController
     netflix = Rails.application.credentials.unogsng_key
 
     begin
+      response = RestClient.get('https://unogsng.p.rapidapi.com/search?type=movie&orderby=rating',
+                                headers = { 'x-rapidapi-key': netflix })
+      higher_rated_movies = netflix_sort(response)
+
       if params.has_key?(:lat) && params.has_key?(:lon)
-        country_results = Geocoder.search([params[:lat], params[:lon]])
-        country = country_results.first.country
-
-        response = RestClient.get('https://unogsng.p.rapidapi.com/search?type=movie&orderby=rating',
-                                  headers = { 'x-rapidapi-key': netflix })
-        movie_array = JSON.parse(response.body)['results']
-
-        movie_results = movie_array.select { |movie| !movie['clist'].include?(country) }
-        render json: { body: movie_results[0..9]}, status: 200
-
-      elsif response = RestClient.get('https://unogsng.p.rapidapi.com/search?type=movie&orderby=rating',
-                                      headers = { 'x-rapidapi-key': netflix })
-        results = JSON.parse(response)['results'].select { |film| film['avgrating'] > 4 }
-
-        netflix_sorted = results.sort_by { |film| film['avgrating'] }.reverse
-        render json: { body: netflix_sorted[0..9] }, status: 200
+        country = get_country(params)
+        movie_results = higher_rated_movies.select { |movie| !movie['clist'].include?(country) }
+        render json: { body: movie_results[0..9] }, status: 200
+      elsif render json: { body: higher_rated_movies[0..9] }, status: 200
       end
     rescue StandardError => e
       render json: { error: e.response.description }, status: e.response.code
     end
+  end
+
+  private
+
+  def get_country(params)
+    country_results = Geocoder.search([params[:lat], params[:lon]])
+    country_results.first.country
+  end
+
+  def netflix_sort(list_of_movies)
+    results = JSON.parse(list_of_movies)['results'].select { |film| film['avgrating'] > 4 }
+    results.sort_by { |film| film['avgrating'] }.reverse
   end
 end
