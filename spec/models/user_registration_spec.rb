5cc2d81e782066b6e6dd9e1a17622e4f4c2131d1
luke <lukeperrin27@gmail.com>
2021-05-08 15:08:10 +0200

    setup for User factory bot and unit testing

---
+++ spec/models/user_registration_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe User, type: :model do
+  describe 'db:table' do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+  end
+
+  describe "factory" do
+    it 'is expected to have valid factory' do
+      expect(create(:user)).to be_valid 
+    end
+  end
+  
+end
